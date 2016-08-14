b031100c6075d25058d0e1661711902f9e97a331
nalexeric <me@ericnastase.com>
2016-08-14 13:41:07 +0300

    integrate google analyitcs on the server

--- db.go
+++ db.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"os"
 	"database/sql"
 	_ "github.com/mattn/go-sqlite3"
 	"log"
-	"fmt"
 )
 
 func Connect() (db *sql.DB) {
-	if _, err := os.Stat("/loc.db"); os.IsNotExist(err) {
-  	fmt.Println("existaaaa")
-	}
 	db, err := sql.Open("sqlite3", "loc.db")
 	if err != nil {
 		log.Fatal(err)
--- logger.go
+++ logger.go
@@ -4,13 +4,22 @@ import (
 	"log"
 	"net/http"
 	"time"
+  "github.com/jpillora/go-ogle-analytics"
 )
 
 func Logger(inner http.Handler, name string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-
 		inner.ServeHTTP(w, r)
+		
+		client, err := ga.NewClient("UA-67119922-8")
+    if err != nil {
+      panic(err)
+    }
+    err = client.Send(ga.NewEvent(name, r.RequestURI))
+    if err != nil {
+        panic(err)
+    }
 
 		log.Printf(
 			"%s\t%s\t%s\t%s",
