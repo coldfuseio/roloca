c46eebbaac11b1dc3a59cdf1c32b39fd4f12596b
nalexeric <me@ericnastase.com>
2016-08-15 15:11:08 +0300

    ro version fixes

--- main.go
+++ main.go
@@ -30,6 +30,7 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
             <meta name="keywords" content="open api, romania, romanian, cities, counties">
             <meta name="author" content="coldfuse.io">
             <meta name="viewport" content="width=device-width, initial-scale=1">
+						<link rel="alternate" hreflang="ro" href="http://roloca.coldfuse.io/ro">
 
             <!-- Place favicon.ico in the root directory -->
             <link rel="shortcut icon" href="http://coldfuse.io/favicon.ico" />
@@ -45,7 +46,7 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
             <div class="u-fpc">
                 <div class="u-fpc-content">
 
-                    <img src="http://coldfuse.io/coldfuse-logo.png" alt="coldfuse logo" class="logo center">
+                    <img src="http://coldfuse.io/logos/regular/mono-inverted/coldfuseLogo.svg" alt="coldfuse logo" class="logo center">
                     <div class="logoType">
                         roloca
                     </div>
@@ -112,11 +113,12 @@ func HomeRoHandler(w http.ResponseWriter, r *http.Request) {
         <head>
             <meta charset="utf-8">
             <meta http-equiv="x-ua-compatible" content="ie=edge">
-            <title>Roloca: an open API for romanian counties and cities</title>
-            <meta name="description" content="An open API for romanian addresses which lists all counties in Romania, all cities in Romania and all cities in a romanian county, developed at coldfuse.io">
-            <meta name="keywords" content="open api, romania, romanian, cities, counties">
+            <title>Roloca: un API public pentru localităţile şi judeţele din România</title>
+            <meta name="description" content="API public ce conţine lista tuturor judeţelor din România şi lista tuturor oraşelor din România, organizate pe judeţe; dezvoltat de coldfuse.io">
+            <meta name="keywords" content="open api, românia, romanesti, orase, judete">
             <meta name="author" content="coldfuse.io">
             <meta name="viewport" content="width=device-width, initial-scale=1">
+						<link rel="alternate" hreflang="en" href="http://roloca.coldfuse.io">
 
             <!-- Place favicon.ico in the root directory -->
             <link rel="shortcut icon" href="http://coldfuse.io/favicon.ico" />
@@ -132,7 +134,7 @@ func HomeRoHandler(w http.ResponseWriter, r *http.Request) {
             <div class="u-fpc">
                 <div class="u-fpc-content">
 
-                    <img src="http://coldfuse.io/coldfuse-logo.png" alt="coldfuse logo" class="logo center">
+                    <img src="http://coldfuse.io/logos/regular/mono-inverted/coldfuseLogo.svg" alt="coldfuse logo" class="logo center">
                     <div class="logoType">
                         roloca
                     </div>
@@ -140,29 +142,28 @@ func HomeRoHandler(w http.ResponseWriter, r *http.Request) {
                     <!-- Add your site or application content here -->
                     <div class="container--text center">
 
-
-                        <h1 class="u-tac h-p u-mb40">An open API for Romanian counties and cities</h1>
+                        <h1 class="u-tac h-p u-mb40">API public pentru localităţile şi judeţele din România</h1>
 
                         <div class="u-mb20">
-                            <h2 class="h-p">All counties in Romania</h2>
+                            <h2 class="h-p">Toate judeţele din România</h2>
                             <a href="/judete" target="_blank" class="code-link">roloca.coldfuse.io/judete</a>
                         </div>
 
                         <div class="u-mb20">
-                            <h2 class="h-p">All cities in Romania</h2>
+                            <h2 class="h-p">Toate oraşele din România</h2>
                             <a href="/orase" target="_blank" class="code-link">roloca.coldfuse.io/orase</a>
                         </div>
 
                         <div class="u-mb40">
-                            <h2 class="h-p">All cities in a county</h2>
+                            <h2 class="h-p">Toate oraşele dintr-un anumit judeţ</h2>
                             <a href="/orase/GL" target="_blank" class="code-link">roloca.coldfuse.io/orase/GL</a>
                         </div>
 
-                        <p class="u-tac u-mb40">Drop us a line at<a href="mailto:hello@coldfuse.io">hello@coldfuse.io</a>for any suggestions/issues</p>
+                        <p class="u-tac u-mb40">Trimite-ne un email la <a href="mailto:hello@coldfuse.io">hello@coldfuse.io</a> pentru probleme / sugestii</p>
 
 
                         <p class="u-tac">
-                            developed at <a href='http://coldfuse.io'>coldfuse.io</a><br>using data from <a href='https://github.com/romania/localitati'>github.com/romania/localitati</a>
+                            dezvoltat de <a href='http://coldfuse.io'>coldfuse.io</a><br> folosind date de la <a href='https://github.com/romania/localitati'>github.com/romania/localitati</a>
                         </p>
 
 
