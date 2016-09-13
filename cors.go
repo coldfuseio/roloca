45cc5f938542bc8df17e51bf3a230a87223aa28c
nalexeric <me@ericnastase.com>
2016-09-13 13:48:41 +0300

    fix remove }

--- cors.go
+++ cors.go
@@ -7,7 +7,7 @@ func corsHandler(fn http.HandlerFunc) http.HandlerFunc {
         w.Header().Set("Access-Control-Allow-Origin", "*")
         w.Header().Set("Access-Control-Allow-Methods", "GET")
         w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token")
-        w.Header().Set("Access-Control-Allow-Credentials", "true")        }
+        w.Header().Set("Access-Control-Allow-Credentials", "true")
         fn(w, r)
     }
 }
