21e3f603bb4a2fccd1a2afbedab2dcefc1cd84c4
nalexeric <me@ericnastase.com>
2017-01-26 23:53:47 +0200

    allow all origins

--- cors.go
+++ cors.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func corsHandler(fn http.HandlerFunc) http.HandlerFunc {
     return func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Access-Control-Allow-Origin", "*")
+        w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
         w.Header().Set("Access-Control-Allow-Methods", "GET")
         w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token")
         w.Header().Set("Access-Control-Allow-Credentials", "true")
